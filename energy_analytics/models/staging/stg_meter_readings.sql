9b2a88fbf91e576faa2305644c77701d395e0b0f
Wan-Hua Hsieh <ehsieh0715@gmail.com>
2026-08-18 12:07:24 +0100

    Add source freshness monitor

--- energy_analytics/models/staging/stg_meter_readings.sql
+++ energy_analytics/models/staging/stg_meter_readings.sql
@@ -3,6 +3,7 @@ select
     meter_id,
     cast(reading_timestamp as timestamp) as reading_timestamp,
     cast(consumption_kwh as decimal(10, 2)) as consumption_kwh,
-    {{ normalize_text('reading_type') }} as reading_type
+    {{ normalize_text('reading_type') }} as reading_type,
+    cast(loaded_at as timestamp) as loaded_at
 
 from {{ source('raw', 'meter_readings') }}
